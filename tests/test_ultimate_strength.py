2493423544fa03db815ce70c0e1b3c33afb7caa2
Vladimir <dan1997ford@yandex.ru>
2022-11-24 13:58:29 +0300

    Add test, add data for 440 degree

--- tests/test_ultimate_strength.py
+++ tests/test_ultimate_strength.py
@@ -1,9 +1,19 @@
 from ultimate_strength import UltimateStrength
-
+import numpy as np
 def test_09G2S():
     us = UltimateStrength()
     assert us.get_ultimate_strength('09Г2С', 175)
 
-def test_St3():
+def test_St3_410():
     us = UltimateStrength()
     assert us.get_ultimate_strength('Ст3', 410, thin=False, durability=20) == 65
+
+def test_St3_430():
+    us = UltimateStrength()
+    assert us.get_ultimate_strength('Ст3', 430, thin=False, durability=20) == 57
+
+def test_09G2S_440():
+    us = UltimateStrength()
+    assert us.get_ultimate_strength('09Г2С', 440, thin=True, durability=20) == 66
+
+
