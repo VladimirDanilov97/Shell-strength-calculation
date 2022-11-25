9af432f02844b41c4e02033bdc9a4661c05d4d52
Vladimir <dan1997ford@yandex.ru>
2022-11-25 16:41:26 +0300

    Add bottom route

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from flask import Flask, request, render_template
-from calculator import Shell
-from forms import ShellForm
+from calculator import Shell, ElepticBottom
+from forms import ShellForm, BottomForm
 app = Flask(__name__)
 app.config.from_object('config')
 
@@ -25,8 +25,29 @@ def shell():
         context['d0r'] = sh.calculate_unreinforced_hole()
         context['k_zap'] = sh.calculate_k_zap()
     return render_template('shell.html', **context)
+ 
     
-
+@app.route('/bottom', methods=['GET','POST'])
+def shell():
+    form = BottomForm()
+    context = {'form': form, 'Sr': None, 'd0r': None, 
+               'k_zap': None, 'title': "Расчет обечайки"}
+ 
+    if form.validate_on_submit():
+        bt = ElepticBottom()
+        bt.set_P(float(form.P.data))
+        bt.set_T(float(form.T.data))
+        bt.set_Dvn(float(form.Dvn.data))
+        bt.set_steel(form.steel.data)
+        bt.set_S(float(form.S.data))
+        bt.set_C(float(form.C.data))
+        bt.set_phi(float(form.phi.data))
+        bt.set_H(float(form.H.data))
+        Sr = bt.calculate_Sr()
+        context['Sr'] = Sr
+        context['d0r'] = bt.calculate_unreinforced_hole()
+        context['k_zap'] = bt.calculate_k_zap()
+    return render_template('shell.html', **context)
 
 if __name__ == '__main__':
     app.run(debug=True)
