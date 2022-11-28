80d8d83ae94ba0f3cce2ad8f0c6991b4e991c1d6
Vladimir <dan1997ford@yandex.ru>
2022-11-28 09:07:25 +0300

    Add positive number validator

--- forms.py
+++ forms.py
@@ -1,21 +1,26 @@
 from flask_wtf import FlaskForm
 from wtforms import DecimalField, SelectField
-from wtforms.validators import DataRequired
+from wtforms.validators import DataRequired, NumberRange, ValidationError
+
 
 
 class ShellForm(FlaskForm): # Добавить валидатор положительного значения! # Добавить валидатор S > C
-    P = DecimalField('P', validators = [DataRequired()]) # Давление внутреннее 
-    Dvn = DecimalField('Dvn', validators = [DataRequired()]) # Диаметр внутренний
-    T = DecimalField('T', validators = [DataRequired()]) # Расчетная температура
-    S = DecimalField('S', validators = [DataRequired()]) # Толщина стенки
-    phi = DecimalField('phi', validators = [DataRequired()]) # Коэффициент прочности сварного шва 
-    C = DecimalField('C', validators = [DataRequired()]) # Суммарна прибавка к толщине стенки
+    
+    P = DecimalField('P', validators = [DataRequired(), NumberRange(min=0.01)]) # Давление внутреннее 
+    Dvn = DecimalField('Dvn', validators = [DataRequired(), NumberRange(min=0.01)]) # Диаметр внутренний
+    T = DecimalField('T', validators = [DataRequired(), NumberRange(min=0.01, max=430)]) # Расчетная температура
+    S = DecimalField('S', validators = [DataRequired(), NumberRange(min=0.01)]) # Толщина стенки
+    phi = DecimalField('phi', validators = [DataRequired(), NumberRange(min=0.01)]) # Коэффициент прочности сварного шва 
+    C = DecimalField('C', validators = [DataRequired(), NumberRange(min=0.01)]) # Суммарна прибавка к толщине стенки
     steel = SelectField('steel',
             choices=[('09Г2С', '09Г2С'),('Ст3', 'Ст3')],
             validators = [DataRequired()]) # Материал 
 
+    
+        
+
 
 class BottomForm(ShellForm):
-    H = DecimalField('C', validators = [DataRequired()]) # Высота днища
+    H = DecimalField('C', validators = [DataRequired(), NumberRange(min=0.01)]) # Высота днища
 
     
